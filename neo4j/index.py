eb10b2c28dd403d1c0101f1d356b5188d2f6aca7
Ryan <ryanarajo@yahoo.com.br>
2024-07-02 18:34:28 -0300

    fix: connection

--- neo4j/index.py
+++ neo4j/index.py
@@ -1,240 +1,184 @@
-from neo4j import GraphDatabase, basic_auth
+import random
+from neo4j import GraphDatabase
 
-class Neo4jConnection:
+class App:
     def __init__(self, uri, user, password):
-        self._driver = GraphDatabase.driver(uri, auth=basic_auth(user, password))
+        self.driver = GraphDatabase.driver(uri, auth=(user, password))
 
     def close(self):
-        self._driver.close()
+        self.driver.close()
 
-    def execute_query(self, query, params=None):
-        with self._driver.session(database="neo4j") as session:
-            result = session.run(query, params)
-            return result
+    def criar_usuario(self):
+        with self.driver.session(database="neo4j") as session:
+            session.execute_write(self._criar_usuario)
 
-
-uri = "neo4j+s://8d6c4351.databases.neo4j.io"
-user = "neo4j"
-password = "22ttZiKIwGGOccOjbyg2nBZlWt5iKp789NXJ4GFj0tc"
-conn = Neo4jConnection(uri, user, password)
-
-class HandlerVendedor:
     @staticmethod
-    def insert():
+    def _criar_usuario(tx):
         query = """
-        CREATE (vendedor:Vendedor {nome: $nome, cpf_cnpj: $cpf_cnpj, telefone: $telefone, email: $email,
-                                    cep: $cep, numero_casa: $numero_casa, rua: $rua, bairro: $bairro,
-                                    cidade: $cidade, estado: $estado})
-        RETURN vendedor
+        CREATE (u:Usuario {nome: $nome, email: $email, cpf: $cpf, estado: $estado, cidade: $cidade, rua: $rua, numero: $numero})
         """
-        print("### Informações pessoais ###")
-        nome = input("Nome do vendedor: ")
-        cpf_cnpj = input("CPF ou CNPJ: ")
-        print("### Informações para contato ###")
-        telefone = input("Telefone: ")
-        email = input("Email: ")
-        cep = input("CEP: ")
-        numero_casa = input("Numero da casa: ")
-        rua = input("Rua: ")
-        bairro = input("Bairro: ")
-        cidade = input("Cidade: ")
-        estado = input("Estado: ")
-        params = {
-            "nome": nome,
-            "cpf_cnpj": cpf_cnpj,
-            "telefone": telefone,
-            "email": email,
-            "cep": cep,
-            "numero_casa": numero_casa,
-            "rua": rua,
-            "bairro": bairro,
-            "cidade": cidade,
-            "estado": estado
+        usuario = {
+            'nome': input("Nome do usuário: "),
+            'email': input("Email: "),
+            'cpf': input("CPF: "),
+            'estado': input("Estado: "),
+            'cidade': input("Cidade: "),
+            'rua': input("Rua: "),
+            'numero': input("Número: ")
         }
-        result = conn.execute_query(query, params)
-        return result.single()
+        tx.run(query, **usuario)
+        print("Usuário criado com sucesso!")
+
+    def criar_vendedor(self):
+        with self.driver.session(database="neo4j") as session:
+            session.execute_write(self._criar_vendedor)
 
     @staticmethod
-    def findAll():
+    def _criar_vendedor(tx):
         query = """
-        MATCH (vendedor:Vendedor)
-        RETURN vendedor
+        CREATE (v:Vendedor {nome: $nome, email: $email, cnpj: $cnpj})
         """
-        result = conn.execute_query(query)
-        return result
-
-
-class HandlerCliente:
-    @staticmethod
-    def insert():
-        query = """
-        CREATE (cliente:Cliente {nome: $nome, cpf: $cpf, data_nascimento: $data_nascimento, 
-        telefone: $telefone, email: $email, cep: $cep, numero_casa: $numero_casa, rua: $rua,
-        bairro: $bairro, cidade: $cidade, estado: $estado, favoritos: []})
-        RETURN cliente
-        """
-        print("### Informações pessoais ###")
-        nome = input("Nome do cliente: ")
-        data_nascimento = input("Data de nascimento no formato DD/MM/AAAA: ")
-        cpf = input("CPF: ")
-        print("### Informações para contato ###")
-        telefone = input("Telefone: ")
-        email = input("Email: ")
-        cep = input("CEP: ")
-        numero_casa = input("Numero da casa: ")
-        rua = input("Rua: ")
-        bairro = input("Bairro: ")
-        cidade = input("Cidade: ")
-        estado = input("Estado: ")
-        params = {
-            "nome": nome,
-            "cpf": cpf,
-            "data_nascimento": data_nascimento,
-            "telefone": telefone,
-            "email": email,
-            "cep": cep,
-            "numero_casa": numero_casa,
-            "rua": rua,
-            "bairro": bairro,
-            "cidade": cidade,
-            "estado": estado
+        vendedor = {
+            'nome': input("Nome do vendedor: "),
+            'email': input("Email: "),
+            'cnpj': input("CNPJ: ")
         }
-        result = conn.execute_query(query, params)
-        return result.single()
+        tx.run(query, **vendedor)
+        print("Vendedor criado com sucesso!")
+
+    def criar_produto(self):
+        with self.driver.session(database="neo4j") as session:
+            session.execute_write(self._criar_produto)
 
     @staticmethod
-    def findAll():
+    def _criar_produto(tx):
         query = """
-        MATCH (cliente:Cliente)
-        RETURN cliente
+        MATCH (v:Vendedor {email: $email_vendedor})
+        CREATE (p:Produto {nome: $nome, descricao: $descricao, preco: $preco})
+        CREATE (v)-[:VENDE]->(p)
         """
-        result = conn.execute_query(query)
-        return result
-
-
-class HandleProduto:
-    @staticmethod
-    def insert():
-        nome = input("Nome do produto: ")
-        descricao = input("Descricao do produto: ")
-        preco = float(input("Preco do produto: "))
-        query = """
-        CREATE (produto:Produto {nome: $nome, descricao: $descricao, preco: $preco)
-        RETURN produto
-        """
-        params = {
-            "nome": nome,
-            "descricao": descricao,
-            "preco": preco
+        produto = {
+            'nome': input("Nome do produto: "),
+            'descricao': input("Descrição: "),
+            'preco': input("Preço: "),
+            'email_vendedor': input("Email do vendedor: ")
         }
-        result = conn.execute_query(query, params)
-        return result.single()
+        tx.run(query, **produto)
+        print("Produto criado com sucesso e relacionamento estabelecido com o vendedor!")
+
+    def realizar_compra(self):
+        with self.driver.session(database="neo4j") as session:
+            session.execute_write(self._realizar_compra)
 
     @staticmethod
-    def findAll():
-        query = """
-        MATCH (produto:Produto)
-        RETURN produto
-        """
-        result = conn.execute_query(query)
-        return result
+    def _realizar_compra(tx):
+        compra_id = str(random.randint(1, 100000))
+        compra = {
+            'id': compra_id,
+            'produto': input("Nome do produto: "),
+            'vendedor': input("Email do vendedor: "),
+            'usuario': input("Email do usuário: ")
+        }
 
-
-class HandleCompra:
-    @staticmethod
-    def selectCliente(self, id_cliente):
         query = """
-        MATCH (cliente:Cliente {id: $id_cliente})
-        RETURN cliente
+        MATCH (u:Usuario {email: $usuario})
+        MATCH (v:Vendedor {email: $vendedor})
+        MATCH (p:Produto {nome: $produto})
+        CREATE (c:Compra {id: $id})
+        CREATE (u)-[:REALIZOU]->(c)
+        CREATE (v)-[:VENDEU]->(c)
+        CREATE (p)-[:INCLUI]->(c)
         """
-        params = {"id_cliente": id_cliente}
-        result = conn.execute_query(query, params)
-        return result.single()
-
-    @staticmethod
-    def selectProduto(self, id_produto):
-        query = """
-        MATCH (produto:Produto {id: $id_produto})
-        RETURN produto
-        """
-        params = {"id_produto": id_produto}
-        result = conn.execute_query(query, params)
-        return result.single()
-
-    @staticmethod
-    def insert():
-        query = """
-        MATCH (cliente:Cliente {id: $id_cliente})
-        MATCH (produto:Produto {id: $id_produto})
-        CREATE (compra:Compra {
-            id_cliente: $id_cliente,
-            id_produto: $id_produto,
-            quantidade: $quantidade,
-            valor_total: $valor_total,
-            data: date()
-        })
-        MERGE (cliente)-[:REALIZOU_COMPRA]->(compra)
-        MERGE (produto)-[:FOI_COMPRADO_EM]->(compra)
-        RETURN compra
-        """
-        id_cliente = input("Informe o ID do cliente: ")
-        cliente = HandleCompra.selectCliente(id_cliente)
-        if not cliente:
-            print(f"Cliente com ID {id_cliente} não encontrado.")
-            return None
         
-        id_produto = input("Informe o ID do produto: ")
-        produto = HandleCompra.selectProduto(id_produto)
-        if not produto:
-            print(f"Produto com ID {id_produto} não encontrado.")
-            return None
-        else:
-            quantidade = int(input("Informe a quantidade do produto: "))
-            valor_total = produto["preco"]*quantidade
-        params = {
-            "id_cliente": id_cliente,
-            "id_produto": id_produto,
-            "quantidade": quantidade,
-            "valor_total": valor_total
-        }
-        result = conn.execute_query(query, params)
-        return result.single()
+        tx.run(query, **compra)
+        print("Compra realizada com sucesso!")
+
+    def procurar_usuarios(self):
+        with self.driver.session(database="neo4j") as session:
+            session.read_transaction(self._procurar_usuarios)
 
     @staticmethod
-    def findAll():
-        query = """
-        MATCH (compra:Compra)
-        RETURN compra
-        """
-        result = conn.execute_query(query)
-        return result
+    def _procurar_usuarios(tx):
+        query = "MATCH (u:Usuario) RETURN u"
+        result = tx.run(query)
+        for row in result:
+            print(row)
+
+    def procurar_vendedores(self):
+        with self.driver.session(database="neo4j") as session:
+            session.read_transaction(self._procurar_vendedores)
+
+    @staticmethod
+    def _procurar_vendedores(tx):
+        query = "MATCH (v:Vendedor) RETURN v"
+        result = tx.run(query)
+        for row in result:
+            print(row)
+
+    def procurar_produtos(self):
+        with self.driver.session(database="neo4j") as session:
+            session.read_transaction(self._procurar_produtos)
+
+    @staticmethod
+    def _procurar_produtos(tx):
+        query = "MATCH (p:Produto) RETURN p"
+        result = tx.run(query)
+        for row in result:
+            print(row)
+
+    def procurar_compras(self):
+        with self.driver.session(database="neo4j") as session:
+            session.read_transaction(self._procurar_compras)
+
+    @staticmethod
+    def _procurar_compras(tx):
+        query = "MATCH (c:Compra) RETURN c"
+        result = tx.run(query)
+        for row in result:
+            print(row)
 
 
-def menu_principal():
+def menu(app):
     while True:
-        print("\n=== MENU PRINCIPAL ===")
-        print("1 - Cadastrar Cliente")
-        print("2 - Cadastrar Vendedor")
-        print("3 - Cadastrar Produto")
-        print("4 - Cadastrar Compra")
-        print("5 - Listar Clientes")
-        print("6 - Listar Vendedores")
-        print("7 - Listar Produtos")
-        print("8 - Listar Compras")
-        print("0 - Sair")
-        opcao = int(input("Selecione a opção desejada: "))
-        if opcao == 1: HandlerCliente.insert() 
-        elif opcao == 2: HandlerVendedor.insert()
-        elif opcao == 3: HandleProduto.insert()
-        elif opcao == 4: HandleCompra.insert()
-        elif opcao == 5: HandlerCliente.findAll()
-        elif opcao == 6: HandlerVendedor.findAll()
-        elif opcao == 7: HandleProduto.findAll()
-        elif opcao == 8: HandleCompra.findAll()
-        elif opcao == 0:
-            print("Tchau, até mais!")
+        print("""
+        1 - Novo Usuario
+        2 - Novo Vendedor
+        3 - Novo Produto
+        4 - Nova Compra
+        5 - Listar todos Usuarios
+        6 - Listar todos Vendedores
+        7 - Listar todos Produtos
+        8 - Listar todas Compras
+        0 - Sair
+        """)
+        escolha = input("Digite a Operação desejada: ")
+        if escolha == '1':
+            app.criar_usuario()
+        elif escolha == '2':
+            app.criar_vendedor()
+        elif escolha == '3':
+            app.criar_produto()
+        elif escolha == '4':
+            app.realizar_compra()
+        elif escolha == '5':
+            app.procurar_usuarios()
+        elif escolha == '6':
+            app.procurar_vendedores()
+        elif escolha == '7':
+            app.procurar_produtos()
+        elif escolha == '8':
+            app.procurar_compras()
+        elif escolha == '0':
+            print("Até a Próxima!")
+            app.close()
             break
-        else: print("Opção inválida. Por favor, escolha uma opção válida.")
+        else:
+            print("Operação não entendida")
+
 
 if __name__ == "__main__":
-    menu_principal()
+    uri = "neo4j+s://0b4d57fa.databases.neo4j.io:7687"
+    user = "neo4j"
+    password = "7ORJYUD_V3JlwHLlZuo19KJkCAPWNAiDXX4vn6VgyRQ"
+    app = App(uri, user, password)
+    menu(app)
